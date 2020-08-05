507c53f606fbb87dcfa2f78a80d2d15b08bdd965
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-05 18:19:01 -0400

    add hash to browser and hide panels

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 document.querySelector('form').addEventListener('submit', (event) => {
   event.preventDefault();
 
+  document.querySelector('#message-form').classList.add('hide');
+  document.querySelector('#link-form').classList.remove('hide');
+
   // Take value and turn into base64
   const input = document.querySelector('#message-input');
   const encrypted = btoa(input.value);
